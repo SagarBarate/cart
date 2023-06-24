219c42a551fa2828aaded1466841098f5712ff39
Sagar Barate <sagarbarate2907@com.com>
2023-06-24 17:31:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const na = "hjvj";
+  return (
+    <div className="App">
+      <h1 >
+        Cart {na}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
