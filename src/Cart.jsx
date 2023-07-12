f809160f06832bec9868c3ecedaee7675a9c0227
Sagar Barate <90105721+SagarBarate@users.noreply.github.com>
2023-07-12 08:15:30 +0530

    Update Cart.jsx

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -4,12 +4,10 @@ import CartItem from './CartItem';
 // props are similar to arguments 
 
 const Cart = (props) => {
-    
-    
         const { products } = props;
+
         return(
             <div className='cart'>
-                
                 {products.map ((product) => {
                     return (
                         <CartItem 
